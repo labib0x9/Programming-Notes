0d36e297642099e3dcbbaa026d4911021e8324b4
labib0x0hunter <labibfaisal450@gmail.com>
2026-02-02 15:21:03 +0600

    program execition using exec.Command()

---
+++ GO-System-01/01 - Program Execution/basic.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	cmd := exec.Command("./sum") // sum is a ext file in the current dir with no arg
+
+	// io.Write implements
+	// stores output in buf
+	out := strings.Builder{}
+
+	// io.Reader implements
+	in := strings.NewReader("1 2\n")
+
+	// I/O mechanism
+	cmd.Stdout = &out
+	cmd.Stdin = in
+
+	// runs a program and wait to finish
+	// Run() = Start() + Wait()
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Run Error" + err.Error())
+		return
+	}
+
+	fmt.Println(out.String())
+
+	//		Same - but error because cmd can be run only once.
+	// For multiple runnig copy the struct into another	variable
+	//
+	out.Reset()
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Start Error: " + err.Error())
+		return
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Wait Error: " + err.Error())
+		return
+	}
+
+	fmt.Println(out.String())
+}
