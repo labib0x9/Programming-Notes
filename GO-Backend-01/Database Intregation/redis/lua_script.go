d2f1b0da9f4b4cb96df34154e1ebab71be3afabc
labib0x0hunter <labibfaisal450@gmail.com>
2026-04-17 15:32:40 +0600

    fix: key missing, value set to false automatically

--- GO-Backend-01/Database Intregation/redis/lua_script.go
+++ GO-Backend-01/Database Intregation/redis/lua_script.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
+
+// data becomes false if key is missing
+// data is stored value if key isn't missing.
 
 var luaCode = `
 	local key = KEYS[1]
 	local value = ARGV[1]
 	local data = redis.call("GET", key)
 
-	if data == nil then
+	if data == false then
 		redis.call("SET", key, value)
 		redis.call("PEXPIRE", key, 6000)
 		return {0, value}
 	end
 
-	return {1, data}
+	return {1, tostring(data)}
 `
 
 var script = redis.NewScript(luaCode)
@@ -33,6 +37,10 @@ func main() {
 
 	ctx := context.Background()
 
+	if err := client.Ping(ctx).Err(); err != nil {
+		panic(err)
+	}
+
 	key := "test:localhost:user-agent"
 
 	res, err := script.Run(
@@ -43,12 +51,21 @@ func main() {
 	).Result()
 
 	if err != nil {
+		panic(err)
 	}
 
 	data := res.([]interface{})
 	allowed := data[0].(int64)
 	result := data[1].(string)
 
-	_ = allowed
-	_ = result
+	// _ = allowed
+	// _ = result
+
+	if allowed == 1 {
+		fmt.Println("OK")
+		fmt.Println(result)
+		fmt.Printf("RAW: %#v, TYPE: %T\n", data[1], data[1])
+	} else {
+		fmt.Println("NOT OK")
+	}
 }
